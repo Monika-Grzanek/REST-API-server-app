c57059469bbf64714f1169864cdb2e79d5350f58
Monika Grzanek <monski@o2.pl>
2022-03-10 14:04:55 +0100

    Changes in server

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -20,7 +20,7 @@ router.route('/seats').post((req, res) => {
   const { day, seat, client, email } = req.body;
  
   if (db.seats.some(item => (item.day == day && item.seat == seat))) {
-    res.status(400).send(`The slot is already taken...`);
+    res.status(400).json({message:`The slot is already taken...`});
   }
   else {
     db.seats.push({
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-var cors = require('cors')
+const cors = require('cors');
+const path = require('path');
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -10,14 +11,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors())
 
-app.use('/api', testimonialsRoutes);
-app.use('/api', concertsRoutes);
-app.use('/api', seatsRoutes);
+app.use('/', testimonialsRoutes);
+app.use('/', concertsRoutes);
+app.use('/', seatsRoutes);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+    app.use(express.static(path.join(__dirname, '/client/build')));
+});
 
 app.use((req, res) => {
     res.status(404).send('404 not found...');
 })
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
 });
