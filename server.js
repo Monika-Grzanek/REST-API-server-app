6d799deafec9215b09a80c70b4188a753aae442d
Monika Grzanek <monski@o2.pl>
2022-03-21 18:36:58 +0100

    Connect server with mongoose

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 const socket = require('socket.io');
+const mongoose = require('mongoose');
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -11,6 +12,14 @@ const seatsRoutes = require('./routes/seats.routes');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', err => console.log('Error ' + err));
 
 const server = app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
